c2e636e03e97e50aac71d957dec6e01c100c48ba
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-01 14:55:15 +0200

    Add project files.

---
+++ Method Array List of Custom Type/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Method_Array_List_of_Custom_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.DoSomething();
+        }
+        public void DoSomething()
+        {
+            User user = new User();
+            user.FirstName = "Krzysztof";
+            user.LastName = "Bojarczuk";
+
+            User user2 = new User();
+            user2.FirstName = "Tomek";
+
+            List<User> users = new List<User>();
+            users.Add(user);
+            users.Add(user2);
+
+            User.PrintUsers(users);
+        }
+    }
+}
---
+++ Method Array List of Custom Type/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Array_List_of_Custom_Type
+{
+    class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public string Output(int times)
+        {
+            string message = "";
+            for (int i = 0; i < times; i++)
+            {
+                message += FullName + "\n";
+            }
+            return message;
+        }
+
+        public static void PrintUsers(List<User> users)
+        {
+            foreach (User user in users)
+            {
+                Console.WriteLine(user.Output(1));
+            }
+        }
+        public static void PrintUser(User user)
+        {
+            Console.WriteLine("Static method. Print User");
+            Console.WriteLine(user.Output(1));
+        }
+    }
+}
